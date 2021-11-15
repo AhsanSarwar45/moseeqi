81756f59d08c1490d20079891981a10e5162e638
AhsanSarwar45 <ahsanrockzs@gmail.com>
2021-11-15 22:31:20 +0500

    Add incomplete add user to database functionality

--- README.md
+++ README.md
@@ -9,3 +9,19 @@
 7. Run `npm install` to install all client packages 
 8. Start the client using `npm start`
 9. Client will start at `http://localhost:3000`. Open this address in the browser to view it.
+
+## Import the schema
+If you want to import the current version of the Moseeqi schema:
+
+1. Open MySQL Workbench
+2. Create a schema named `moseeqi`
+3. Go to `Server > Data Import`
+4. Choose the `Import from Self-Contained File` option
+5. Browse to this project directory and select `moseeqi.sql` under `server/schema` 
+6. Click `Start Import`
+
+## Automatic server restart
+If you want the Node server to restart automatically after every save: 
+
+1. Install nodemon using `npm install nodemon`
+2. Instead of `npm start`, use `nodemon index.js`
--- client/src/components/TextInput.jsx
+++ client/src/components/TextInput.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { useBoolean, Button, VStack, Text, InputGroup, Input, InputRightElement } from '@chakra-ui/react';
 
-export const TextInput = ({label}) => {
+export const TextInput = ({label, value, onChange, type}) => {
 	return (
 		<VStack w="300px" align="left">
 			<Text ml="18px">{label}</Text>
-			<Input variant="filled" />
+			<Input type = {type} value = {value} onChange = {onChange} variant = "filled" />
 		</VStack>
 	);
 };
 
-export const PasswordInput = ({label}) => {
+TextInput.defaultProps = {
+  label: "Input",
+  type: "",
+}
+ 
+
+export const PasswordInput = ({label, value, onChange}) => {
 	const [ show, setShow ] = useBoolean();
 
 	return (
 		<VStack w="300px" align="left">
 			<Text ml="18px">{label}</Text>
 			<InputGroup size="md">
-				<Input variant="filled" type={show ? 'text' : 'password'} />
+				<Input variant="filled" value = {value} onChange = {onChange} type={show ? 'text' : 'password'} />
 				<InputRightElement width="4.5rem">
 					<Button h="1.75rem" size="sm" onClick={setShow.toggle}>
 						{show ? 'Hide' : 'Show'}
