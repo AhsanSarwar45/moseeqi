7fed2e6a5962a0a697b9c33c63bbac2658fa8b04
Ahmad <23100222@lums.edu.pk>
2021-11-18 17:41:57 +0500

    Sign up Success Page merged

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -4,6 +4,7 @@ import {About} from './routes/About';
 import {Home} from './routes/Home';
 import {Login} from './routes/Login';
 import {User} from './routes/User';
+import {SignUpSuccess} from './routes/SignUpSuccess';
 import {Search} from './routes/Search';
 
 export const Main = () => (
@@ -13,6 +14,7 @@ export const Main = () => (
     <Route path='/signup' element={<SignUp/>}/>
     <Route path='/login' element={<Login/>}/>
     <Route path='/user' element={<User/>}/>
+    <Route path='/signup_success' element={<SignUpSuccess/>}/>
     <Route path='/search' element={<Search/>}/>
     </Routes>
 );
