4b2036aabe4f3191f74dfb1ce5c2ca9f1335194a
AhsanSarwar45 <ahsanrockzs@gmail.com>
2021-11-19 15:44:22 +0500

    Remove redundant profile page

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,22 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
-import {SignUp} from './routes/SignUp';
-import {About} from './routes/About';
-import {Home} from './routes/Home';
-import {Login} from './routes/Login';
-import {User} from './routes/User';
-import {SignUpSuccess} from './routes/SignUpSuccess';
-import {Search} from './routes/Search';
-import { ViewProfile } from './routes/ViewProfile';
+import { SignUp } from './routes/SignUp';
+import { About } from './routes/About';
+import { Home } from './routes/Home';
+import { Login } from './routes/Login';
+import { User } from './routes/User';
+import { SignUpSuccess } from './routes/SignUpSuccess';
+import { Search } from './routes/Search';
+import { Profile } from './routes/Profile';
 
 export const Main = () => (
-    <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/about' element={<About/>}/>
-    <Route path='/signup' element={<SignUp/>}/>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/user' element={<User/>}/>
-    <Route path='/signup_success' element={<SignUpSuccess/>}/>
-    <Route path='/search' element={<Search/>}/>
-    <Route path='/view-profile' element={<ViewProfile/>}/>
-    </Routes>
+	<Routes>
+		<Route path="/" element={<Home />} />
+		<Route path="/about" element={<About />} />
+		<Route path="/signup" element={<SignUp />} />
+		<Route path="/login" element={<Login />} />
+		<Route path="/user" element={<User />} />
+		<Route path="/signup_success" element={<SignUpSuccess />} />
+		<Route path="/search" element={<Search />} />
+		<Route path="/profile" element={<Profile />} />
+	</Routes>
 );
--- client/src/routes/Profile.jsx
+++ client/src/routes/Profile.jsx
@@ -1,9 +1,20 @@
-import {Navbar} from "../components/NavBar";
+import { Link } from 'react-router-dom';
+import { NavbarUser } from '../components/NavBarUser';
+import { Spacer, Button, HStack, VStack, Heading, Container, Text } from '@chakra-ui/react';
 
-export const Profile = () => (
-    <div className='profile'>
-    <Navbar/>
-    Profile
-    </div>
-);
+export const Profile = () => {
+	let data = sessionStorage.getItem('user-data');
+	data = JSON.parse(data);
+	console.log(data);
 
+	return (
+		<div>
+			<NavbarUser />
+			<VStack padding={0} spacing={5}>
+				<Heading size="md">Profile Info:</Heading>
+				<Text> Phone Number: {data.phone_number} </Text>
+				<Text> Password: {data.password} </Text>
+			</VStack>
+		</div>
+	);
+};
