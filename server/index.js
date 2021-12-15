c7ea38279b0d81bc0cf5cf824e9e4ef6ed635b9d
Omer Kamran <omer.kamran100@gmail.com>
2021-12-15 14:20:12 +0500

    cros fixed

--- server/index.js
+++ server/index.js
@@ -18,23 +18,23 @@ app.use(express.json());
 app.use(fileUpload());
 app.use(express.static('data'));
 
-app.use(function(req, res, next) {
-	// Website you wish to allow to connect
-	res.setHeader('Access-Control-Allow-Origin', '*');
+// app.use(function(req, res, next) {
+// 	// Website you wish to allow to connect
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
 
-	// Request methods you wish to allow
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// 	// Request methods you wish to allow
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-	// Request headers you wish to allow
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+// 	// Request headers you wish to allow
+// 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
 
-	// // Set to true if you need the website to include cookies in the requests sent
-	// // to the API (e.g. in case you use sessions)
-	// res.setHeader('Access-Control-Allow-Credentials', true);
+// 	// // Set to true if you need the website to include cookies in the requests sent
+// 	// // to the API (e.g. in case you use sessions)
+// 	// res.setHeader('Access-Control-Allow-Credentials', true);
 
-	// Pass to next layer of middleware
-	next();
-});
+// 	// Pass to next layer of middleware
+// 	next();
+// });
 
 let users = []; //for seeding
 
