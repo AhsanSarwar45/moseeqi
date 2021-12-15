76d73f6795c05d5d7c8dd9f6cfec940e9edfbe35
Omer Kamran <omer.kamran100@gmail.com>
2021-12-15 08:51:45 +0500

    pp

--- server/index.js
+++ server/index.js
@@ -16,24 +16,24 @@ app.use(express.json());
 app.use(fileUpload());
 app.use(express.static('data'));
 
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
 
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', 'https://moseeqi.vercel.app/');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://moseeqi.vercel.app/');
 
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-//     // Pass to next layer of middleware
-//     next();
-// });
+    // Pass to next layer of middleware
+    next();
+});
 
 // const initiliazeTables = `
 
