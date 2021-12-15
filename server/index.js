4e4525cc68caa8de20e064873f19b1d042218734
Omer Kamran <omer.kamran100@gmail.com>
2021-12-15 09:00:47 +0500

    /

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.static('data'));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://moseeqi.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://moseeqi.vercel.app');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
